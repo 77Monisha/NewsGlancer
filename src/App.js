622aa2deb70a79abd7685d3ca15c710e1a159304
Monisha Chaurasia <monisha.chaurasia@xarterian.com>
2026-08-17 18:01:22 +0530

    chore: code refactor & added env

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
-import React from 'react'
-import Navbar from './components/Navbar';
-import News from './components/News';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+const categories = [
+  "business",
+  "entertainment",
+  "general",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
 
-const App = ()=> {
+const App = () => {
   const pageSize = 15;
   const apiKey = process.env.REACT_APP_NEWS_API;
-  
+  // NewsAPI's free plan only serves top-headlines for a handful of countries;
+  // 'in' now returns totalResults: 0, so default to 'us'.
+  const country = process.env.REACT_APP_NEWS_COUNTRY || "us";
 
-    return (
-      <div>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<News apiKey={apiKey} key="general" pageSize={pageSize} country='in' category='general' />} />
-            <Route path="/business" element={<News apiKey={apiKey} key="business" pageSize={pageSize} country='in' category='business' />} />
-            <Route path="/entertainment" element={<News apiKey={apiKey} key="entertainment" pageSize={pageSize} country='in' category='entertainment' />} />
-            <Route path="/general" element={<News apiKey={apiKey} key="general" pageSize={pageSize} country='in' category='general' />} />
-            <Route path="/health" element={<News apiKey={apiKey} key="health" pageSize={pageSize} country='in' category='health' />} />
-            <Route path="/science" element={<News apiKey={apiKey} key="science" pageSize={pageSize} country='in' category='science' />} />
-            <Route path="/sports" element={<News apiKey={apiKey} key="sports" pageSize={pageSize} country='in' category='sports' />} />
-            <Route path="/technology" element={<News apiKey={apiKey} key="technology" pageSize={pageSize} country='in' category='technology' />} />
-          </Routes>
-        </BrowserRouter>
+  const newsFor = (category) => (
+    <News
+      apiKey={apiKey}
+      key={category}
+      pageSize={pageSize}
+      country={country}
+      category={category}
+    />
+  );
 
-      </div>
-    )
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={newsFor("general")} />
+          {categories.map((category) => (
+            <Route
+              key={category}
+              path={`/${category}`}
+              element={newsFor(category)}
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
-
-
-
